f973a6185c030f2af93b611652817b2a6ebedbef
Pranjali tupat <pranjalitupat04@gmail.com>
2024-12-09 09:55:51 +0530

    test

--- element.js
+++ element.js
@@ -1,9 +1,8 @@
 let arr=[2,4,6,7,9,5];
 let element = 9;
 for(let i=0;i<arr.length;i++){
-    console.log(arr[i]+arr[i]);
+    console.log('arr[i]'+arr[i]);
     if(arr[i]==element){
-        console.log('element fount is arr[i]'+at+)
+        console.log('element fount is'+arr[i]+'at index'+i);
     }
-    
 }
